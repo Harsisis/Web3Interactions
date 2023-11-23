82e8d44491f77aa2b90a0dc3573fe06574c821d3
Haorsis <cadetgauthier3@gmail.com>
2023-11-23 14:18:26 +0100

    add token

--- script.js
+++ script.js
@@ -28,14 +28,27 @@ const minABI = [
     outputs: [{ name: "", type: "string" }],
     type: "function",
   },
+  // decimals
+  {
+    constant: true,
+    inputs: [],
+    name: "decimals",
+    outputs: [{ name: "", type: "uint8" }],
+    type: "function",
+  },
 ];
+
 const CONTRACT_MT_ADDRESS = "0x2E7DA19698F7184f238c6A1bEA52D2e645965396";
 const CONTRACT_SOPH_ADDRESS = "0xC1932768A7453c493861E7D5551164878DEcE3d2";
+const CONTRACT_FR_ADDRESS = "0xD8f3c097471B621378D3729e1cC4961aC36A3356";
+const CONTRACT_BALD_ADDRESS = "0x622bA4BA3b03C653aFD77ee1d605061C8d69817c";
 
 const contractMT = new web3Client.eth.Contract(minABI, CONTRACT_MT_ADDRESS);
 const contractSOPH = new web3Client.eth.Contract(minABI, CONTRACT_SOPH_ADDRESS);
+const contractFR = new web3Client.eth.Contract(minABI, CONTRACT_FR_ADDRESS);
+const contractBALD = new web3Client.eth.Contract(minABI, CONTRACT_BALD_ADDRESS);
 
-const contractList = [contractMT, contractSOPH];
+const contractList = [contractMT, contractSOPH, contractFR, contractBALD];
 
 const MMSDK = new MetaMaskSDK.MetaMaskSDK();
 
@@ -64,8 +77,20 @@ const getBalance = async (contract, wallet) => {
   const result = await contract.methods.balanceOf(wallet).call();
   const name = await contract.methods.name().call();
   const symbol = await contract.methods.symbol().call();
+  const decimals = await contract.methods.decimals().call();
 
-  console.log(`Balance in ${name}: ${result} ${symbol}`);
+  const amount = decimalsToUnits(result, decimals);
+
+  console.log(`Balance in ${name}: ${amount} ${symbol}`);
+};
+
+const decimalsToUnits = (tokenUnits, decimals) => {
+  let decimalsInt = parseInt(decimals);
+  let tokenUnit =
+    tokenUnits.toString().slice(0, -decimalsInt) +
+    "." +
+    tokenUnits.toString().slice(-decimalsInt);
+  return parseFloat(tokenUnit);
 };
 
 const retrieveTokenForLoggedUser = async () => {
